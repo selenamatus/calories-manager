147e95c3d324cff7c8cdc142586be77f5b2c558a
Selena Metosevich <SELENAM@ariel-wimasor.co.il>
2024-07-17 17:37:12 +0300

    Update project with latest changes

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -2,8 +2,9 @@
 
 exports.getAbout = (req, res) => {
   const developers = [
-    { firstname: "dave", lastname: "cohen", id: 234234, email: "daddd@gmail.com" },
-    { firstname: "tal", lastname: "levy", id: 34534544, email: "tal@gmail.com" }
+    { firstname: "Selena", lastname: "Matusevich", id: 206511503, email: "matusevich.selena@gmail.com" },
+    { firstname: "Daniella", lastname: "Aharonov", id: 314750621, email: "daniellaaharonov@gmail.com" }, 
+    { firstname: "ofek", lastname: "daida", id: 315143958, email: "ofekdaida16@gmail.com"}
   ];
 
   res.json(developers); // Sending the developer information as a response
